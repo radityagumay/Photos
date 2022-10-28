f8fbb4b8028856474ac7eccc71e7fa66f658eeaa
radityagumay <gumay.raditya@gmail.com>
2022-10-28 10:10:59 +0700

    fix kotlin version issue

--- app/src/main/java/com/radityalabs/photos/MainActivity.kt
+++ app/src/main/java/com/radityalabs/photos/MainActivity.kt
@@ -3,41 +3,25 @@ package com.radityalabs.photos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.radityalabs.photos.ui.theme.PhotosTheme
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PhotosTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Magenta)
+            ) {
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PhotosTheme {
-        Greeting("Android")
     }
 }
